cd8c9a7d76369c67b0dcbb67e6ca8272aae30f7f
Frank Mullenger <frank@silverstripe.com>
2013-11-14 21:41:01 +1300

    NEW: Using yaml configuration files.

--- _config.php
+++ _config.php
@@ -1,11 +1,1 @@
 <?php
-
-//Extensions
-Object::add_extension('ShopConfig', 'Coupon_Extension');
-Object::add_extension('Order', 'Coupon_OrderExtension');
-Object::add_extension('OrderForm', 'Coupon_CheckoutFormExtension');
-Object::add_extension('CheckoutPage_Controller', 'CouponModifierField_Extension');
-
-if (class_exists('ExchangeRate_Extension')) {
-	Object::add_extension('Coupon', 'ExchangeRate_Extension');
-}
